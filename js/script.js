723864e6e6346c71a8e6788c2e3acb24eab970ef
Luan Santos <jhonatanluanoficial@gmail.com>
2021-10-28 15:59:36 -0300

    Validation Loading....

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
+const validation = document.querySelector('.main__wrapper__validation__box');
 const cpf = document.querySelector('.main__wrapper__validation__box-cpf');
 const button = document.querySelector('.main__wrapper__validation__btn')
+const paragraph = document.createElement('p')
 
-button.addEventListener('click', () =>{
-    alert(cpf.value)
+// Create Tag Paragraph
+paragraph.classList.add('main__wrapper__validation__paragraph')
+validation.appendChild(paragraph)
+
+// Function of click 
+button.addEventListener('click', () => {
+    requiredField(),
+    formatCPF()
 });
 
-$(document).ready(() =>{
+
+// Formater CPF Number
+$(document).ready(() => {
     $(cpf).mask("000.000.000-00")
 });
 
-console.log(cpf.value)
+function requiredField(){
+    if(cpf.innerHTML === ''){
+        paragraph.innerHTML = 'Preencha o campo'
+        return
+    }
+}
+
+function formatCPF(){
+    if (cpf.value.length != 14) {
+        
+        paragraph.innerHTML = 'Insira um formato CPF';
+        return
+    }
+}
